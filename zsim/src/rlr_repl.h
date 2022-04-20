5466188855142665a1583d81564eaa5aa3d54c08
Jason-Li <17914+Jason-Li@users.noreply.github.tamu.edu>
2022-04-20 14:48:21 -0500

    revert to unoptimized implementation

--- zsim/src/rlr_repl.h
+++ zsim/src/rlr_repl.h
@@ -11,17 +11,17 @@ memreq struct definition in memory_hierarchy.h, contains prefetch info
 //RLR
 class RLRReplPolicy : public ReplPolicy {
     protected:
-		//per-cache-line variables
+		//cache line variables
         uint32_t* ageCounter;
         bool* hitReg;
         bool* typeReg;
 
-		//per-set variables
+		//set variables
 		uint32_t* RD;
 		uint32_t* accum;
-		uint32_t* missCounter;
+		uint32_t* hitCounter;
 		
-		//constants
+		//cache variables
 		bool hit;
 		uint32_t numLines;
 		uint32_t candidates;
@@ -31,64 +31,68 @@ class RLRReplPolicy : public ReplPolicy {
     public:
         // add member methods here, refer to repl_policies.h
         explicit RLRReplPolicy(uint32_t _numLines, uint32_t _candidates) : hit(true), numLines(_numLines), candidates(_candidates){
-			//info("nlines:%d ncand:%d", numLines, candidates);
             ageCounter = gm_calloc<uint32_t>(numLines);
             hitReg = gm_calloc<bool>(numLines);
             typeReg = gm_calloc<bool>(numLines);
 			
 			RD = gm_calloc<uint32_t>(numLines/candidates);
 			accum = gm_calloc<uint32_t>(numLines/candidates);
-			missCounter = gm_calloc<uint32_t>(numLines/candidates);
+			hitCounter = gm_calloc<uint32_t>(numLines/candidates);
         }
 		
         ~RLRReplPolicy() {
             gm_free(ageCounter);
             gm_free(hitReg);
             gm_free(typeReg);
+			gm_free(RD);
+			gm_free(accum);
+			gm_free(hitCounter);
         }
 		
 		void update(uint32_t id, const MemReq* req) {
+			//type register
 			bool isPrefetch = req->flags & MemReq::PREFETCH;
-			//info("isPrefetch %d", isPrefetch);
-			
 			if (isPrefetch) typeReg[id]=0;
 			else typeReg[id]=1;
 			
+			//hit register
 			hitReg[id] = hit;
+			
+			if(hit){
+				//age counter
+				uint32_t setIdx=id/candidates;
+				uint32_t begin = setIdx*candidates;
+				for(uint32_t i=begin;i<begin+candidates;i++)
+					if(ageCounter[i]<32)
+						ageCounter[i]++;
+				accum[setIdx] += ageCounter[id];
+				ageCounter[id] = 0;
+				
+				//RD
+				hitCounter[setIdx]++;
+				if(hitCounter[setIdx]==32){
+					RD[setIdx] = accum[setIdx] >> 4;
+					accum[setIdx] = 0;
+					hitCounter[setIdx] = 0;
+					//info("set %d, updated RD:%d", setIdx, RD[setIdx]);
+				}
+			}
 			hit = true;
 		}
 		
 		void replaced(uint32_t id) {
 			hit = false;
-			accum[id/candidates] += ageCounter[id];
 			ageCounter[id] = 0;
 		}
 		
 		template <typename C> inline uint32_t rank(const MemReq* req, C cands) {
-			assert(*cands.end()-*cands.begin()==candidates);
-			uint32_t setIdx = *cands.begin()/candidates;
-			//info("Handling cache miss at set %d", setIdx);
-			//info("r:%d a:%d m:%d", RD[setIdx], accum[setIdx], missCounter[setIdx]);
-			
-			missCounter[setIdx]++;
-			if(missCounter[setIdx]==8) {
-				//info("r:%d a:%d m:%d", RD[setIdx], accum[setIdx], missCounter[setIdx]);
-				for(auto ci = cands.begin(); ci != cands.end(); ci.inc())
-					if(ageCounter[*ci]<4)
-						ageCounter[*ci]++;
-				RD[setIdx] = accum[setIdx] >> 1;
-				accum[setIdx] = 0;
-				missCounter[setIdx] = 0;
-			}
-			
-			
 			uint32_t bestCand = -1;
 			uint32_t Pa,Ph,Pt = 0;
 			uint32_t Pline;
 			uint32_t minP = (1>>31)-1;
 			
 			for (auto ci = cands.begin(); ci != cands.end(); ci.inc()) {
-				if(ageCounter[*ci] > RD[setIdx])Pa = 0;
+				if(ageCounter[*ci] > RD[*ci/candidates])Pa = 0;
 				else Pa = 8;
 
 				if(hitReg[*ci]) Ph = 1;
@@ -97,7 +101,11 @@ class RLRReplPolicy : public ReplPolicy {
 				if(typeReg[*ci]) Pt = 1;
 				else Pt = 0;
 				
-				Pline = Pa+Pt+Ph;
+				Pline = (Pa+Pt+Ph)*2;
+				
+				//Recency approximation
+				if(ageCounter[*ci]==0) Pline--;
+				
 				//info("Cand %d have priority of %d", *ci, Pline);
 				if(Pline < minP) {
 					minP = Pline;
